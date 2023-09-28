e3e45f72bb8243787e9be98af015626b73a21fa4
felipeescalada <felipeescalada@gmail.com>
2023-09-28 15:30:48 -0500

    otro comit 26

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ const corsOptions = {
   },
   credentials: true
 };
-
+//cross domain.
 var allowCrossDomain = function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', '"GET,PUT,PATCH,POST,DELETE"');

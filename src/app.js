6f6a5f69d916cc4b29919f8d5324c79994a822c9
felipeescalada <felipeescalada@gmail.com>
2023-09-28 15:19:46 -0500

    paquete 23

--- src/app.js
+++ src/app.js
@@ -9,13 +9,16 @@ const corsOptions = {
   },
   credentials: true
 };
-app.use(cors(corsOptions));
+
 var allowCrossDomain = function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type,token');
   next();
 }
+
+app.use(allowCrossDomain);
+
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET,PUT,PATCH,POST,DELETE");

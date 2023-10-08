fe161dfa62cb67287be5472540a80be7ed4017d8
fescalada@rimith.com <felipeescalada@gmail.com>
2023-10-08 17:10:15 -0500

    el log no se.

--- src/app.js
+++ src/app.js
@@ -91,9 +91,9 @@ app.get('/usuarios2', (req, res) => {
 
 app.post('/api/getusuario23', (req, res) => {
   logger.log("info", "Request received en el body:"  + req.body ,   req.body.usuario +"//" + req.body);
-  //const [rows] = await pool.query('SELECT * FROM user WHERE Username="' + req.body.usuario +'"');
-  //res.json(req.body);
-  res.send({ status: req.body });
+  const [rows] =  pool.query('SELECT * FROM user WHERE Username="' + req.body.usuario +'"');
+  res.json(req.body);
+  //res.send({ status: req.body });
 });
 
 

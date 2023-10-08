af8f3329688e8064e831d590a8591030da088001
fescalada@rimith.com <felipeescalada@gmail.com>
2023-10-08 12:36:33 -0500

    usuario23

--- src/app.js
+++ src/app.js
@@ -64,10 +64,10 @@ app.get('/usuarios2', (req, res) => {
   });
 });
 
-app.post('/api/getusuario', async (req, res) => {
+app.post('/api/getusuario23', async (req, res) => {
   console.log(`usuario: ${req.body.usuario}`);
-  const [rows] = await pool.query(`SELECT * FROM user WHERE Username="${req.body.usuario}"`)
-  res.json(rows)
+  const [rows] = await pool.query(`SELECT * FROM user WHERE Username="${req.body.usuario}"`);
+  res.json(rows);
 });
 
 

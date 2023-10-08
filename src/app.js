5d1919e0008db15b6ea76ca2893b440470d0fa67
fescalada@rimith.com <felipeescalada@gmail.com>
2023-10-08 17:22:55 -0500

    info

--- src/app.js
+++ src/app.js
@@ -94,7 +94,7 @@ app.post('/api/getusuario23', async (req, res) => {
   const [rows] =  await pool.query('SELECT * FROM user WHERE Username="' + req.body.usuario +'"');
   logger.log({
     level: 'info',
-    message: 'Hello distributed log files!'
+    message: 'Hello distributed log files!' + req.body
   });
   res.json(rows);
   //res.send({ status: req.body });

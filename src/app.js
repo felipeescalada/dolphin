96c33f4bd66aef55abe3b971e7db88c2bd9c65ab
fescalada@rimith.com <felipeescalada@gmail.com>
2023-10-08 16:53:51 -0500

    cambia header

--- src/app.js
+++ src/app.js
@@ -16,7 +16,8 @@ const app = express();
 var allowCrossDomain = function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', '"GET,PUT,PATCH,POST,DELETE"');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  //res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, contentType,Content-Type, Accept, Authorization");
   next();
 }
 //const winston2 = winston();

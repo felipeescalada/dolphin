188d45b3145f17ac696f5412535f8b34836dc11f
fescalada@rimith.com <felipeescalada@gmail.com>
2023-10-08 16:48:38 -0500

    url

--- src/app.js
+++ src/app.js
@@ -32,6 +32,7 @@ const logger = winston.createLogger({
 
 logger.log("info", "Request received en el body 1:",   "1");
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }))
 logger.log("info", "Request received en el body 2:",   "2");
 
 app.use(allowCrossDomain);

ce6fcc3bf8c1788cf738e73e9dfac91be896ad69
fescalada@rimith.com <felipeescalada@gmail.com>
2023-10-08 16:04:26 -0500

    test

--- src/app.js
+++ src/app.js
@@ -5,12 +5,12 @@ import { pool } from './db.js';
 import {PORT} from './config.js';
 
 
-//var bodyParser = require('body-parser');
+
 const app = express();
 
 
 
-app.use(bodyParser.json);
+
 app.use(bodyParser.urlencoded({ extended: true }))
 
 //cross domain.

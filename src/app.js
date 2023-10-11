2f7725de6d770f3501e81a873d703f71b81dc555
fescalada@rimith.com <felipeescalada@gmail.com>
2023-10-10 19:20:51 -0500

    cajasmov

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import { pool } from './db.js';
-import {PORT} from './config.js';
+import { PORT } from './config.js';
 import cors from 'cors';
 
 
@@ -10,12 +10,12 @@ app.use(bodyParser.json());
 
 //este es cors
 app.use(cors({
-    origin: '*'
+  origin: '*'
 }));
 //app.use(bodyParser.urlencoded({ extended: true }))
 
 //cross domain.
-var allowCrossDomain = function(req, res, next) {
+var allowCrossDomain = function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', '"GET,PUT,PATCH,POST,DELETE"');
   //res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
@@ -35,9 +35,9 @@ app.use(allowCrossDomain);
 //felipe cambios domingo 08 oct 2023.
 
 app.get('/alumnos', async (req, res) => {
-  const [rows] = await pool.query('select idAlumno,Nombre,Apellido,last_update,substring(FechaNac,1,10) FechaNac,'+
-  ' idSede,idCurso,Estado,Email,Sexo,coalesce(idprovincia,0) idprovincia,' +
-  ' coalesce(iddistrito,0) iddistrito,coalesce(idcorregimiento,0) idcorregimiento,Direccion,telefono,idpais from alumnos');
+  const [rows] = await pool.query('select idAlumno,Nombre,Apellido,last_update,substring(FechaNac,1,10) FechaNac,' +
+    ' idSede,idCurso,Estado,Email,Sexo,coalesce(idprovincia,0) idprovincia,' +
+    ' coalesce(iddistrito,0) iddistrito,coalesce(idcorregimiento,0) idcorregimiento,Direccion,telefono,idpais from alumnos');
   res.json(rows);
 })
 
@@ -53,19 +53,19 @@ app.post('/api/usuarios', async (req, res) => {
 
 
 app.get('/alumnos2', (req, res) => {
-  
+
   pool.getConnection((err, connection) => {
     if (err) throw err;
     console.log(`#-Conectado como el ID ${connection.threadId}`);
 
-    connection.query('select idAlumno,Nombre,Apellido,last_update,substring(FechaNac,1,10) FechaNac,'+
-    ' idSede,idCurso,Estado,Email,Sexo,coalesce(idprovincia,0) idprovincia,' +
-    ' coalesce(iddistrito,0) iddistrito,coalesce(idcorregimiento,0) idcorregimiento,Direccion,telefono,idpais from alumnos', (err, rows) => {
-      connection.release(); // Devolver la conexión al pool
+    connection.query('select idAlumno,Nombre,Apellido,last_update,substring(FechaNac,1,10) FechaNac,' +
+      ' idSede,idCurso,Estado,Email,Sexo,coalesce(idprovincia,0) idprovincia,' +
+      ' coalesce(iddistrito,0) iddistrito,coalesce(idcorregimiento,0) idcorregimiento,Direccion,telefono,idpais from alumnos', (err, rows) => {
+        connection.release(); // Devolver la conexión al pool
 
-      if (err) throw err;
-      res.send(rows);
-    });
+        if (err) throw err;
+        res.send(rows);
+      });
   });
 });
 
@@ -85,7 +85,7 @@ app.get('/usuarios2', (req, res) => {
 
 app.post('/api/getusuario23', async (req, res) => {
 
-  const [rows] =  await pool.query('SELECT * FROM user WHERE Username="' + req.body.usuario +'"');
+  const [rows] = await pool.query('SELECT * FROM user WHERE Username="' + req.body.usuario + '"');
 
   res.json(rows);
   //res.send({ status: req.body });
@@ -119,37 +119,48 @@ app.get('/', async (req, res) => {
 })
 
 app.get('/terceros', async (req, res) => {
-  const [rows] = await pool.query('select idtercero,razonsocial,nit,direccionfiscal,direccion '+
-    'idciudad,naturaleza,autoretenedor,estado,pnombre,snombre,papellido,sapellido,'+
+  const [rows] = await pool.query('select idtercero,razonsocial,nit,direccionfiscal,direccion ' +
+    'idciudad,naturaleza,autoretenedor,estado,pnombre,snombre,papellido,sapellido,' +
     ' coalesce(idprovincia,0) idprovincia,' +
-    ' coalesce(iddistrito,0) iddistrito,coalesce(idcorregimiento,0) idcorregimiento ,'+
-     ' idlista from tercero');
+    ' coalesce(iddistrito,0) iddistrito,coalesce(idcorregimiento,0) idcorregimiento ,' +
+    ' idlista from tercero');
 
-     res.json(rows);
+  res.json(rows);
 
-  
-  });
 
-  app.post('/productos', async (req, res) => {
-    const [rows] = await pool.query('select * from productos order by item');
-  
-       res.json(rows);
-  
-    
-    });
+});
 
-    app.post('/cajas', async (req, res) => {
-      console.log("cajas server");
-      const [rows] = await pool.query('select idcaja, nombre from  cajas');
-    
-         res.json(rows);
-    
-      
-      });
-  
-   
+app.post('/productos', async (req, res) => {
+  const [rows] = await pool.query('select * from productos order by item');
 
-  
+  res.json(rows);
+
+
+});
+
+app.post('/cajas', async (req, res) => {
+  console.log("cajas server");
+  const [rows] = await pool.query('select idcaja, nombre from  cajas');
+
+  res.json(rows);
+
+
+});
+
+app.post('/cajasmov', async (req, res) => {
+  console.log("cajas server");
+  const [rows] = await pool.query('select idCajaMov, Nombre,last_update,FechaCreacion,FechaCierre,SaldoInicial,Ingreso,Egreso,Saldo,idCaja,idUser from cajasmov where idcaja=' + req.body.pidcaja);
+
+
+  res.json(rows);
+
+
+});
+
+
+
+
+
 
 
 

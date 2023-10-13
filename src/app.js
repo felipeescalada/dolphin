e50ba8ffa818a315f20dac8d8bd0d8499bdc32cb
fescalada@rimith.com <felipeescalada@gmail.com>
2023-10-13 11:02:16 -0500

    app/files

--- src/app.js
+++ src/app.js
@@ -28,7 +28,7 @@ var allowCrossDomain = function (req, res, next) {
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, './current-disk/files/');
+    cb(null, './app/files/');
   },
   filename: (req, file, cb) => {
     // const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);

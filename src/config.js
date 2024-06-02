1126015a2f568d45d6ff0adcc4eb99f4f3202616
felipeescalada <felipeescalada@gmail.com>
2024-06-02 14:55:54 -0500

    no se si va el config

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 export const PORT = process.env.PORT || 3000
 
-export const DB_HOST = process.env.DB_HOST || 'viaduct.proxy.rlwy.net'
+export const DB_HOST = process.env.DB_HOST || 'monorail.proxy.rlwy.net'
 export const DB_USER = process.env.DB_USER || 'root'
-export const DB_PASSWORD = process.env.DB_PASSWORD || 'WUcVexMLvkJkAvMUcBFHphGgFwuzzHGV'
-export const DB_NAME = process.env.DB_NAME || 'erpdolphin'
-export const DB_PORT = process.env.DB_PORT || 26147
+export const DB_PASSWORD = process.env.DB_PASSWORD || 'EBRkLVNzVTtrxOEpIaUxePKLxYDaEGiO'
+export const DB_NAME = process.env.DB_NAME || 'railway'
+export const DB_PORT = process.env.DB_PORT || 17361

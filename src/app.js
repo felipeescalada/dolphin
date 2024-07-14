e33cc43a84e03495284263b29ace2695d445944a
felipeescalada <felipeescalada@gmail.com>
2024-07-13 19:05:08 -0500

    recent_files

--- src/app.js
+++ src/app.js
@@ -146,6 +146,11 @@ app.route('/api/getclientes').get( async (req, res) => {
   res.json(rows);
 });
 
+app.route('/api/get_recent_files').get( async (req, res) => {
+  const [rows] = await pool.query('SELECT * FROM recent_files');
+  res.json(rows);
+});
+
 
 app.post('/documentos', async (req, res) => {
   const [rows] = await pool.query('select id,idtercero, tipodoc, path1, path2  from documentos;');
